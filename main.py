3ff3cf6fd7ade160c1552e58e47c2332273f4a1a
Mariana-Guerra <0161358@sesi-es.org.br>
2026-08-05 09:16:52 -0300

    inicio do cód.

---
+++ main.py
@@ -0,0 +1,25 @@
+import csv
+import os
+ 
+ARQUIVO = "livros.csv"
+CAMPOS = ["titulo", "autor", "ano", "isbn", "status"]
+livros = []
+ 
+# FUNÇÕES DE PERSISTÊNCIA
+ 
+def carregar_livros():
+    """Carrega os livros do arquivo CSV ao iniciar o programa"""
+    global livros
+    livros = []
+    if os.path.exists(ARQUIVO):
+        with open(ARQUIVO, "r", encoding="utf-8") as f:
+            leitor = csv.DictReader(f)
+            for linha in leitor:
+                livros.append(linha)
+ 
+def salvar_livros():
+    """Salva a lista de livros no arquivo CSV"""
+    with open(ARQUIVO, "w", encoding="utf-8", newline="") as f:
+        escritor = csv.DictWriter(f, fieldnames=CAMPOS)
+        escritor.writeheader()
+        escritor.writerows(livros)
